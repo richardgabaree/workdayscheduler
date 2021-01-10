3fcd416df4959dde668c2665481001e1b0b1240b
Richard Gabaree <richard.gabaree@gmail.com>
2021-01-10 16:59:51 -0600

    added the time block coloring to java script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {// tells engine to load 1)html & 2)css first.
     //display current day & time.
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
-    //assign saveBtn click listener for user input and time stamp??
+    //assign saveBtn click listener 
     $(".saveBtn").on("click", function () {
         //get nearby values.
         console.log(this);
@@ -12,6 +12,7 @@ $(document).ready(function () {// tells engine to load 1)html & 2)css first.
         localStorage.setItem(time, text);
     })
     //load any saved data from LocalStorage - do this for each hour created. Should follow html 24 hour to 12 hour conversion.
+    $("#hour7 .description").val(localStorage.getItem("hour7"));
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -22,5 +23,34 @@ $(document).ready(function () {// tells engine to load 1)html & 2)css first.
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
 
-    
+    function hourTracker() {
+        
+        var currentHour = moment().hour(); // use of moment.js
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
+
+        // if we've moved past this time, click into css/html given classes of past, present, or future
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+hourTracker(); //re-run function
+})
